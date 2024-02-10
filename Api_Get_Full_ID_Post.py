bd37892af8fdb24a788dca90a591e139ce0a9104
Ngx_Hieeus <116277261+DevHieeus@users.noreply.github.com>
2024-02-10 22:38:05 +0700

    Create Api_Get_Full_ID_Post.py

---
+++ Api_Get_Full_ID_Post.py
@@ -0,0 +1,9 @@
+from ApiFacebook_DevHieeus import ApiFacebook
+from ApiFacebook_DevHieeus import printf
+from config.cookies import COOKIES
+
+FB = ApiFacebook(COOKIES)
+GetAllPost = FB.GetAllPost(type="",user_id="") 
+
+# Api thì phải mua nhé còn code dùng chỉ ngắn như này thôi kk -)))
+# Mua API FACEBOOK Liên Hệ : FB.com/NgxHieeus hoặc FB.com/NgxHieeusSoftware ! (Giá 1xx)
